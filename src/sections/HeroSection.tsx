c9277bc9e617b87e95b749fe3daac322259d7c26
admin <admin@admins-MacBook-Pro.local>
2026-07-28 22:44:23 +0530

    Content: Change hero text from 'I'm Digifox' to 'Digifox'

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -48,7 +48,7 @@ export const HeroSection: React.FC = () => {
         <div className="w-full px-6 sm:px-0 relative z-30">
           <div className="w-full animate-lcp-fast">
             <h1 className="hero-heading font-black uppercase tracking-tight leading-none whitespace-normal sm:whitespace-nowrap w-full text-center text-5xl sm:text-[5.5vw] md:text-[6vw] lg:text-[7vw] mt-10 sm:mt-0">
-              I'm Digifox
+              Digifox
             </h1>
           </div>
         </div>
